3ad8d3b13dc2d81bf06e8ebad86ebc2b60091e5f
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-08-01 10:54:31 +0200

    Introduce Lombok

--- src/main/java/de/ars/schulung/maven/Main.java
+++ src/main/java/de/ars/schulung/maven/Main.java
@@ -3,5 +3,10 @@ package de.ars.schulung.maven;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Car car = Car.builder()
+                .type("Ford")
+                .year(2010)
+                .build();
+        System.out.println(car.getType());
     }
 }
